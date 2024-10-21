a7c83e5e976648cf53f0ada579a5c6cf570cf252
devanshu <devanshukumawat063@gmail.com>
2024-10-21 17:08:40 +0530

    ADD

--- js/js/index.js
+++ js/js/index.js
@@ -1,1 +1,217 @@
-console.log("Hey..🫤")
+// console.log("Hey..🫤")
+
+
+// JavaScript Variables -> Variables are containers for storing values.
+//var
+
+
+// var firstName =  "Devanshu"
+// var firstName = 1234
+// firstName = "Manny"
+// console.log(firstName)
+
+// {
+//     var firstName = "Daisy"
+// }
+
+// console.log(firstName)
+
+
+
+
+//let
+
+// let lastName = "Kumawat"
+// // let lastName = "Manny"
+// lastName = "Sharma"
+// console.log(lastName)
+
+// {
+//     let lastName = "Sharma"
+// }
+
+// console.log(lastName)
+
+//const
+
+// const age = 16
+// // const age = 78
+// age = 89
+// console.log(age)
+
+// {
+//     const age = 78
+// }
+
+// console.log(age)
+
+
+
+
+// let a = 56
+// console.log(a)
+
+// let userName = "MAnny"
+// let 123test = "5678"
+// let User_name = "Kizie"
+// let &Test = "dfghjk"
+// let _name = "Manny"
+
+
+// let firstName = "Manny"
+// let age = 56
+
+
+//String:
+
+
+// let firstName = "Manny"
+// let lastName = 'Sharma'
+// let age = `56`
+
+// console.log("Value:- "+firstName + "    " + "Type:- "+typeof(firstName))
+// console.log(`Value :- ${lastName} Type     :- ${typeof(lastName)} `)
+// console.log(typeof(age))
+
+//Number:
+
+// let age = 56
+// console.log(`Value :- ${age} Type:- ${typeof(age)}`)
+
+// let task = 67.987
+// console.log(`Value :- ${task} Type:- ${typeof(task)}`)
+
+
+
+//Boolean:
+
+// let x = 78
+// let y = 79
+
+// console.log(x==y)
+
+// let isMarried = true
+// console.log(`Is_Married :- ${isMarried} Type:- ${typeof(isMarried)}`)
+
+
+//Array:
+
+// let myArray = ["Manny",67,true,"Sharma"]
+// console.log(myArray)
+
+//keys           0      1   2      3     
+// const isArrayValue = myArray[3]
+// console.log(myArray[2])
+// console.log(isArrayValue)
+
+// console.log(`Type of Array :- ${typeof(myArray)}`)
+
+// const myArray1 = new Array("Manny",67,98,56,false)
+
+// console.log(myArray1)
+
+
+// let a;
+// // a = 16
+// console.log(a)
+
+// const myArray = ["manny",67,true]
+
+// //     index       0     1    2 
+
+// console.log(myArray)
+// console.log(myArray[1] + " Type :- "+typeof(myArray[1]))
+
+// console.log(myArray[2] + " Type :- "+typeof(myArray[2]))
+
+
+// new Array()
+
+
+
+// const studentObj = {
+//     studentName:"Devanshu",
+//     studentRoll_no:45,
+//     studentSection:"C"
+// }
+
+// console.log(studentObj + " Type :- "+ typeof(studentObj))
+
+
+// // console.log(studentObj.studentRoll_no)
+// // console.log(studentObj.studentName)
+
+// const {studentName,studentRoll_no,studentSection} =  studentObj
+
+// console.log(studentName)
+// console.log(studentRoll_no)
+
+
+
+
+// let test = 20+6
+
+// console.log(test)
+
+// let x = "dfgh"
+// let y = ""
+
+// console.log(x+y)
+
+// let x = "dfghj"
+// let y = 5
+
+// console.log(x-y)
+
+// let x = "dfghj"/2
+// console.log(x)
+
+
+// let x = "7"*5
+// console.log(x)
+
+
+// let test = 12345678976543 %8
+
+// console.log(test)
+
+
+// let x = 12
+// x++
+// console.log(x)
+// x--
+// console.log(x)
+
+// let x = "67"
+// let y = 67
+
+// console.log(x==y)
+// console.log(x===y)
+
+
+let age = 18
+
+if(age>=18){
+    console.log("Yes")
+}
+
+else if(age<18){
+    console.log("No")
+}
+
+else{
+    console.log("Else Part")
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
